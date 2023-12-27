09c85c312816f5d40ef0bbc2d092f59a59948d5e
yakupkalayci <ykpklyc18@gmail.com>
2023-12-27 15:21:24 +0300

    update auth pages design

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,35 +1,55 @@
 import AuthForm from "../ui/login/auth-form/AuthForm";
-import styles from '@/app/ui/login/login.module.css';
+import styles from "@/app/ui/login/login.module.css";
+import Link from "next/link";
 
-export type loginTypes = 'isveren' | 'isarayan' | 'default'; 
+export type loginTypes = "isveren" | "isarayan" | "default";
 
 interface LoginProps {
-  params: {},
+  params: {};
   searchParams: {
     type: loginTypes;
-  }
+  };
 }
 
-
-function Login(props:LoginProps) {
+function Login(props: LoginProps) {
   // destruct props
-  const {searchParams} = props;
-  const type = searchParams?.type || 'default';
+  const { searchParams } = props;
+  let type = searchParams?.type || "default";
+
+  // methods
+  const changeType = async () => {
+    "use server";
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <AuthForm type={type}/>
+        <AuthForm type={type} />
       </div>
       <div className={styles.right}>
         <div className={styles.rightContainer}>
           <h1 className={styles.rightHeading}>{type} girişi</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A eos totam laboriosam officiis, impedit voluptate eius! Obcaecati dicta earum soluta tempore totam, dignissimos, autem libero omnis nostrum nulla quod eius.</p>
-          <button className={styles.btn}>{type === 'default' || type === 'isveren' ? 'İş Arayan Olarak Giriş Yap' : 'İşveren Olarak Giriş Yap'}</button>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A eos totam
+            laboriosam officiis, impedit voluptate eius! Obcaecati dicta earum
+            soluta tempore totam, dignissimos, autem libero omnis nostrum nulla
+            quod eius.
+          </p>
+          <Link
+            href={`/login?type=${
+              type === "default" || type === "isarayan" ? "isveren" : "isarayan"
+            }`}
+          >
+            <button className={`${styles.btn} ${styles.btnRight}`}>
+              {type === "default" || type === "isarayan"
+                ? "İşveren Olarak Giriş Yap"
+                : "İş Arayan Olarak Giriş Yap"}
+            </button>
+          </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Login;
--- app/ui/login/auth-form/AuthForm.tsx
+++ app/ui/login/auth-form/AuthForm.tsx
@@ -1,39 +1,71 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import { loginTypes } from "@/app/login/page";
-import styles from './authForm.module.css';
+import styles from "./authForm.module.css";
 
 interface AuthProps {
-  type: loginTypes; 
+  type: loginTypes;
 }
 
 function AuthForm(props: AuthProps) {
-
   // destruct props
   const { type } = props;
 
   // states
-  const [formType, setFormType] = useState<'login' | 'signup'>('login');
+  const [formType, setFormType] = useState<"login" | "signup">("login");
 
   // methods
   const changeFormType = () => {
-    setFormType(prev => prev === 'login' ? 'signup' : 'login');
+    setFormType((prev) => (prev === "login" ? "signup" : "login"));
+  };
+
+  const login = () => {
+    if(type === 'default' || type === 'isarayan') {
+      console.log("İŞARAYAN GİRİŞİ");
+    } else {
+      console.log("İŞVEREN GİRİŞİ");
+    }
+  }
+
+  const signup = () => {
+    if(type === 'default' || type === 'isarayan') {
+      console.log("İŞARAYAN KAYDI");
+    } else {
+      console.log("İŞVEREN KAYDI");
+    }
   }
 
   return (
     <div className={styles.container}>
-      <h1>{formType === 'login' ? 'Giriş Yap' : 'Kayıt Ol'}</h1>
+      <h1>{formType === "login" ? "Giriş Yap" : "Kayıt Ol"}</h1>
       <div className={styles.inputContainer}>
-        <input type="text" name="email" placeholder="Eposta Adresiniz.." className={styles.input}/>
-        <input type="password" name="password" placeholder="Şifre.." className={styles.input}/>
+        <input
+          type="text"
+          name="email"
+          placeholder="Eposta Adresiniz.."
+          className={styles.input}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Şifre.."
+          className={styles.input}
+        />
       </div>
-      <button className={styles.btn}>{formType === 'login' ? 'Giriş Yap' : 'Kayıt Ol'}</button>
+      <button className={styles.btn} onClick={() => formType === 'login' ? login() : signup()}>
+        {formType === "login" ? "Giriş Yap" : "Kayıt Ol"}
+      </button>
       <div className={styles.signupInfo}>
-        {formType === 'login' ? 'Henüz hesabınız yok mu? ' : 'Hesabınız var mı? '}<span onClick={() => changeFormType()}>{formType === 'login' ? 'Hemen Kayıt Olun!' : 'Giriş Yap'}</span>
+        {formType === "login"
+          ? "Henüz hesabınız yok mu? "
+          : "Hesabınız var mı? "}
+        <span onClick={() => changeFormType()}>
+          {formType === "login" ? "Hemen Kayıt Olun!" : "Giriş Yap"}
+        </span>
       </div>
     </div>
-  )
+  );
 }
 
 export default AuthForm;
